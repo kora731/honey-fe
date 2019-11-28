46278b7668e3de4b920c8f8f08f1468db9b7b9ec
Tal <talrasha007@gmail.com>
2019-11-29 00:39:46 +0800

    init route.

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,22 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+        <v-btn href="#/" text>
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+          />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+          <span class="mr-2">Hashedge</span>
+        </v-btn>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn href="#/" text>
-        <span class="mr-2">Home</span>
-        <v-icon>mdi-home</v-icon>
-      </v-btn>
       <v-btn href="#/about" text>
         <span class="mr-2">About</span>
         <v-icon>mdi-open-in-new</v-icon>
