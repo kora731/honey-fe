a655a10d4c108047198fca5b8540677391ebf267
Surong Ruan <surong731@gmail.com>
2019-12-30 22:08:47 -0800

    Update UI

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -3,19 +3,14 @@
     <v-row justify="center">
       <v-col style="width:60px;">
         <v-card style="background: transparent;" elevation="0">
-          <v-card-title class="pb-0">Coin</v-card-title>
+          <v-card-title class="pb-0 body-2">Coin</v-card-title>
           <v-card-text>
             <v-item-group mandatory>
               <v-container>
                 <v-row class="flex-column">
                   <v-col v-for="n in coinTypes" :key="n">
                     <v-item v-slot:default="{ active, toggle }">
-                      <v-card
-                        :color="active ? 'primary' : ''"
-                        class="d-flex align-center"
-                        dark
-                        
-                      >
+                      <v-card :color="active ? 'primary' : ''" class="d-flex align-center" dark>
                         <v-scroll-y-transition>
                           <div class="display-1 flex-grow-1 text-center">{{n}}</div>
                         </v-scroll-y-transition>
@@ -26,7 +21,7 @@
               </v-container>
             </v-item-group>
           </v-card-text>
-          <v-card-title class="pb-0">Duration</v-card-title>
+          <v-card-title class="pb-0 body-2">Duration</v-card-title>
           <v-card-text>
             <v-item-group mandatory>
               <v-container>
@@ -55,11 +50,9 @@
         <v-card v-for="(item, index) in items[duration]" :key="index" class="mb-5" hover="true">
           <v-card-text class="py-1">
             <v-row align="center" justify="space-between">
-              
-                <v-chip class="ma-1" color="accent" text-color="black" label>BEST DEAL</v-chip>
-            
-                <v-chip class="ma-1" color="secondary" text-color="black" label>28% OFF</v-chip>
-              
+              <v-chip class="ma-1" color="accent" text-color="black" label small>BEST DEAL</v-chip>
+
+              <v-chip class="ma-1" color="secondary" text-color="black" label small>28% OFF</v-chip>
             </v-row>
             <v-row align="center" justify="space-between">
               <div class="ma-2">
@@ -73,30 +66,52 @@
             </v-row>
             <v-row align="center">
               <div class="flex-grow-1 ma-2">
-                <div class="caption">Today's Mining Income</div>
+                <div class="caption">
+                  Today's Mining Income
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                      <v-icon v-on="on" class="body-1">mdi-help-circle-outline</v-icon>
+                    </template>
+                    I'm a tooltip
+                  </v-tooltip>
+                </div>
                 <div class="subtitle-1">$1.40</div>
               </div>
               <div class="d-inline-flex ma-2 text-right">
                 <div style="flex-basis:auto;" class="mr-4">
-                  <div class="caption">Upfront Fee</div>
+                  <div class="caption">
+                    Upfront Fee
+                    <v-tooltip bottom>
+                      <template v-slot:activator="{ on }">
+                        <v-icon v-on="on" class="body-1">mdi-help-circle-outline</v-icon>
+                      </template>
+                      I'm a tooltip
+                    </v-tooltip>
+                  </div>
                   <div class="subtitle-1">{{item.commission * 100}}%</div>
                 </div>
                 <div>
-                  <div class="caption">Electricity Fee</div>
+                  <div class="caption">
+                    Electricity Fee
+                    <v-tooltip bottom>
+                      <template v-slot:activator="{ on }">
+                        <v-icon v-on="on" class="body-1">mdi-help-circle-outline</v-icon>
+                      </template>
+                      I'm a tooltip
+                    </v-tooltip>
+                  </div>
                   <div class="subtitle-1">${{item.electricityFee}}/T/D</div>
                 </div>
               </div>
             </v-row>
             <v-row>
               <v-col cols="9">
-                <div>
-                  {{item.seller}}
-                </div>
+                <div>{{item.seller}}</div>
               </v-col>
               <v-col cols="3" class="text-right">
-                <v-btn href="#/product" color="primary">
-                  <v-icon class="mr-2">mdi-open-in-new</v-icon>
-                  <span>Check it out</span>
+                <v-btn href="#/product" color="primary" class="check-btn">
+                  <v-icon>mdi-open-in-new</v-icon>
+                  <span class="ml-2">Check it out</span>
                 </v-btn>
               </v-col>
             </v-row>
@@ -118,10 +133,7 @@ export default {
   data() {
     return {
       duration: "30D",
-      coinTypes: [
-        "BTC",
-        "ETH"
-      ],
+      coinTypes: ["BTC", "ETH"],
       items: {
         "30D": [
           {
@@ -227,16 +239,28 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .v-item--active {
   color: #222;
   background-color: #ffff00 !important;
 }
 
 .v-card--hover:hover {
-  -webkit-box-shadow: 0px 1px 2px 4px rgba(251, 255, 0, 0.3),
-    0px 2px 4px 1px rgba(251, 255, 0, 0.2);
-  box-shadow: 0px 1px 2px 4px rgba(251, 255, 0, 0.3),
-    0px 2px 4px 1px rgba(251, 255, 0, 0.2);
+  -webkit-box-shadow: 0px 1px 1px 2px rgba(251, 255, 0, 0.3),
+    0px 2px 2px 2px rgba(251, 255, 0, 0.2);
+  box-shadow: 0px 1px 1px 2px rgba(251, 255, 0, 0.3),
+    0px 2px 2px 2px rgba(251, 255, 0, 0.2);
+}
+
+.check-btn{
+  span{
+    @media (max-width: 800px) {
+      display: none;
+    }
+  }
+  @media (max-width: 800px) {
+      background: white !important;
+      color: black !important;
+      }
 }
 </style>
