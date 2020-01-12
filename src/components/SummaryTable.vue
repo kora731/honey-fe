9df4bd74d3a0646c8a5f5b458839fafbfb93af93
Surong Ruan <surong731@gmail.com>
2020-01-12 12:00:14 -0800

    home card

--- src/components/SummaryTable.vue
+++ src/components/SummaryTable.vue
@@ -2,7 +2,7 @@
   <v-tabs :centered="true">
     <v-tab v-for="coin of coins" :key="'tab-' + coin">{{coin}}</v-tab>
     <v-tab-item  v-for="coin of coins" :key="'tab-item-' + coin">
-      <v-row class="mx-4 pt-4 d-flex" justify="center">
+      <v-row class="mx-4 pt-4 d-flex font-weight-light" justify="center">
         <div class="mx-4">
           <v-icon>mdi-coin</v-icon>
           {{coin}} price: ${{summary[coin].coinPrice}}
@@ -12,23 +12,23 @@
           <v-icon>mdi-treasure-chest</v-icon>
           {{coin}} mining earnings: ${{summary[coin].maxPayOff.toFixed(4)}} /T/Day ({{summary[coin].maxPayOffBtc.toFixed(8)}} BTC)</div>
       </v-row>
-      <div>
+      <div class="pa-4">
         <v-row
           justify="space-between"
           align-content="center"
           class="caption d-none d-sm-flex"
-          style="margin: 0"
+          style="margin: 0; margin-top: 24px;"
         >
           <v-col cols="3">Contract Duration</v-col>
           <v-col cols="2">Lowest Unit Cost (/T/Day)</v-col>
           <v-col cols="2">Cost Basis</v-col>
-          <v-col cols="3">Expected ROI</v-col>
+          <v-col cols="3" style="text-align:center;">Expected ROI</v-col>
         </v-row>
-        <v-row v-for="(c, idx) in summary[coin].contracts" :key="idx" justify="space-between" align-content="center">
+        <v-row v-for="(c, idx) in summary[coin].contracts" :key="idx" justify="space-between" align-content="center" class="homeDeal">
           <v-col cols="3">{{c.duration}} Days</v-col>
           <v-col cols="2">{{c.contract_cost.toFixed(4)}}(/{{summary[coin].unit}}/Day)</v-col>
           <v-col cols="2">${{(1 / c.mining_payoff_btc) * c.contract_cost | price}}per {{coin}}</v-col>
-          <v-col cols="3">{{(c.mining_payoff / c.contract_cost) -1 | percent}}</v-col>
+          <v-col cols="3" class="dealROI">{{(c.mining_payoff / c.contract_cost) -1 | percent}}</v-col>
         </v-row>
       </div>
     </v-tab-item>
@@ -56,5 +56,17 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+.homeDeal{
+    margin:12px;
+    background: #fafafa;
+    border-radius: 6px;
+}
+.dealROI
+{
+  color: #222;
+  background-color: #FFFACC !important;
+  text-align: center;
+}
 
 </style>
