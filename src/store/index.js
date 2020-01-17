ba74c49e0ac5632798fba3c1fd9e3bf54109c38f
Tal <talrasha007@gmail.com>
2020-01-17 16:44:58 -0500

    duration alias.

--- src/store/index.js
+++ src/store/index.js
@@ -18,6 +18,16 @@ const summaryData = {
   reward: 0
 };
 
+function getDurationAlias(d) {
+  const months = Math.round(d / 30);
+  if (months <= 1) return '1 month';
+  if (months < 12) return months + ' months';
+
+  const years = Math.round(d / 365);
+  if (years === 1) return '1 year';
+  else return years + ' years';
+}
+
 export default new Vuex.Store({
   state: {
     selectedProduct: null,
@@ -37,7 +47,9 @@ export default new Vuex.Store({
   },
   mutations: {
     setProducts(state, products) {
+      products = products.map(p => ({ ...p, durationAlias: getDurationAlias(p.duration) }));
       state.products = products;
+
       products.forEach(p => {
         const summary = state.summary[p.coin];
         if (summary) {
@@ -55,20 +67,20 @@ export default new Vuex.Store({
         }
       });
 
-      const compact = (s, md) => _.chain(s.contracts)
-        .filter(v => v.duration <= md)
-        .sortBy(v => v.duration + (1 - v.expected_discount))
+      const compact = s => _.chain(s.contracts)
+        .sortBy(['durationAlias', 'contract_cost'])
         .sortedUniqBy(v => {
           const ds = s.durationSellers.get(v.duration) || new Set();
           ds.add(v.issuers);
           s.durationSellers.set(v.duration, ds);
-          return v.duration;
+          return v.durationAlias;
         })
+        .sortBy('duration')
         .value();
 
-      state.summary.BTC.contracts = compact(state.summary.BTC, 360);
-      state.summary.ETH.contracts = compact(state.summary.ETH, 2000);
-      state.summary.BCH.contracts = compact(state.summary.BCH, 2000);
+      state.summary.BTC.contracts = compact(state.summary.BTC);
+      state.summary.ETH.contracts = compact(state.summary.ETH);
+      state.summary.BCH.contracts = compact(state.summary.BCH);
     },
     setHashrate(state, o) {
       Object.assign(state.summary[o.coin], o);
@@ -97,19 +109,19 @@ export default new Vuex.Store({
 
       commit('setProducts', data.filter(v => v.sold_percent < 99.99 && lastUpdate - v.update_time < 3600));
 
-      const getChainInfo = async coin => {
-        const { data } = await axios.get(`https://api.i43.io/crypto/data/blockchain/histo/day?fsym=${coin}&limit=3`);
-        if (data.Data) {
-          if (data.Data.Data.length > 2) data.Data.Data.shift();
-          const [d0, d1] = data.Data.Data;
-          const stats = { coin, hashrate: d1.hashrate, reward: d1.current_supply - d0.current_supply };
-          commit('setHashrate', stats);
-        }
-      };
-
-      getChainInfo('BTC');
-      getChainInfo('ETH');
-      getChainInfo('BCH');
+      // const getChainInfo = async coin => {
+      //   const { data } = await axios.get(`https://api.i43.io/crypto/data/blockchain/histo/day?fsym=${coin}&limit=3`);
+      //   if (data.Data) {
+      //     if (data.Data.Data.length > 2) data.Data.Data.shift();
+      //     const [d0, d1] = data.Data.Data;
+      //     const stats = { coin, hashrate: d1.hashrate, reward: d1.current_supply - d0.current_supply };
+      //     commit('setHashrate', stats);
+      //   }
+      // };
+      //
+      // getChainInfo('BTC');
+      // getChainInfo('ETH');
+      // getChainInfo('BCH');
 
       const nicehHashData = async algorithm => {
         const { data } = await axios.get('https://api.i43.io/nicehash/main/api/v2/hashpower/orderBook?algorithm=' + algorithm);
